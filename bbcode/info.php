c57fd5af8df5866ab4c65754232831ce856f44e5
MashaKovalchuk <mashakovalchuk1997@gmail.com>
2017-06-22 23:20:43 +0300

    Add files via upload

---
+++ bbcode/info.php
@@ -0,0 +1,14 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+
+$info = array(
+	'name' => t('BB-коды'),
+	'description' => t('Конвертирует тэги BBCode в HTML'),
+	'version' => '3.1',
+	'author' => 'MAX',
+	'plugin_url' => 'http://max-3000.com/',
+	'author_url' => 'http://maxsite.org/',
+	'group' => 'template',
+	'help' => getinfo('plugins_url') . 'bbcode/bbcode-help.html',
+);
+
+# end file
