05464dde47994324df93ca969bdbf5f1e5eca3f2
MashaKovalchuk <mashakovalchuk1997@gmail.com>
2017-06-22 23:45:04 +0300

    Add files via upload

---
+++ info.php
@@ -0,0 +1,18 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+
+/**
+ * MaxSite CMS
+ * (c) http://max-3000.com/
+ */
+
+$info = array(
+	'name' => 'MAX Zmey',
+	'description' => t('Шаблон для MaxSite CMS'),
+	'version' => '2.0',
+	'author' => 'MAX',
+	'template_url' => 'http://max-3000.com/',
+	'author_url' => 'http://maxsite.org/',
+	'maxsite-min-version' => '0.96'
+);
+
+# end of file
